15433c29de30db86b18a5278def584ea7b05c731
William0832 <squall0832@gmail.com>
2020-11-29 20:41:15 +0800

    finish axios for tgogle favorite and like restaurants

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -16,28 +16,30 @@
       button.btn.btn-danger.btn-border.favorite.mr-2(
         type="button"
         v-if="restaurant.isFavorited"
-        @click.stop.prevent="toggleFavorite"
+        @click.stop.prevent="toggleFavorite(restaurant.id)"
 
       ) 移除最愛
       button.btn.btn-primary.btn-border.favorite.mr-2(
         type="button"
         v-else
-        @click.stop.prevent="toggleFavorite"
+        @click.stop.prevent="toggleFavorite(restaurant.id)"
       ) 加入最愛
       button.btn.btn-danger.btn-border.like.mr-2(
         type="button"
         v-if="restaurant.isLiked"
-        @click.stop.prevent="toggleLike"
+        @click.stop.prevent="toggleLike(restaurant.id)"
       ) Unlike
       button.btn.btn-primary.btn-border.like.mr-2(
         type="button"
         v-else
-        @click.stop.prevent="toggleLike"
+        @click.stop.prevent="toggleLike(restaurant.id)"
       ) Like
 </template>
 
 <script>
 import { emptyImage } from '../utils/mixins'
+import { Toast } from '../utils/helpers'
+import usersAPI from '../apis/users'
 export default {
   mixins: [emptyImage],
   props: {
@@ -50,11 +52,35 @@ export default {
     return { restaurant: this.initialRestaurant }
   },
   methods: {
-    toggleFavorite () {
-      this.restaurant.isFavorited = !this.restaurant.isFavorited
+    async toggleFavorite (restaurantId) {
+      try {
+        this.restaurant.isFavorited = !this.restaurant.isFavorited
+        const { data } = this.restaurant.isFavorited
+          ? await usersAPI.addFavorite({ restaurantId })
+          : await usersAPI.deleteFavorite({ restaurantId })
+        if (data.status !== 'success') throw new Error(data.message)
+      } catch (err) {
+        console.error(err)
+        Toast.fire({
+          icon: 'error',
+          title: '無法改變我的最愛，請稍後再試'
+        })
+      }
     },
-    toggleLike () {
-      this.restaurant.isLiked = !this.restaurant.isLiked
+    async toggleLike (restaurantId) {
+      try {
+        this.restaurant.isLiked = !this.restaurant.isLiked
+        const { data } = this.restaurant.isLiked
+          ? await usersAPI.addLike({ restaurantId })
+          : await usersAPI.deleteLike({ restaurantId })
+        if (data.status !== 'success') throw new Error(data.message)
+      } catch (err) {
+        console.error(err)
+        Toast.fire({
+          icon: 'error',
+          title: '無法修改Like，請收後再試'
+        })
+      }
     }
   }
 }
